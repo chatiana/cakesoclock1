ea32da4c89626c48d833c24f537b67bf3069751b
Chatiana <[thatiana.cm@gmail.com]>
2020-03-27 17:47:29 +0000

    update port display after connection

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const mysql = require("mysql");
 const express = require("express");
 const bodyParser = require("body-parser");
+const router = express.Router();
 
 var app = express();
 app.use(bodyParser.json());
 
+// create connection to database
+// the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
 var mysqlConnection = mysql.createConnection({
     host:"cakesdb1.cvnkqaqbljxc.us-east-1.rds.amazonaws.com",
     user: "admin",
@@ -24,4 +27,9 @@ mysqlConnection.connect((err)=>{
     }
 })
 
-app.listen(3000);
+
+app.use(express.static(__dirname + '/Script'));
+//Store all JS and CSS in Scripts folder.
+app.use('/', router);
+app.listen(process.env.port || 3000);
+console.log('Running at Port 3000');
